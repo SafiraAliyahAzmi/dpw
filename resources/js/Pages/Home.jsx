d6d339ccc913554be32b45ef30e24d67715a82f6
DrRhea <czajr13@gmail.com>
2024-05-27 00:38:11 +0700

    finish creating hero section

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import Main from "../Home/Main";
+import Main from "@/Home/Main";
+import Navbar from "@/Components/Navbar";
 
 const Home = () => {
-    return (
-        <div>
-            Home
-            <Main />
-        </div>
-    );
+  return (
+    <div className="min-h-screen bg-cod-950 text-cod-50 font-urbanist">
+      <Navbar />
+      <Main />
+      <div className="bg-blue-500 h-[700px] flex justify-center items-center">
+        UNDER DEVELOPMENT
+      </div>
+    </div>
+  );
 };
 
 export default Home;
